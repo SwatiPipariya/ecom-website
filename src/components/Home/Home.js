474d177a0721b33d86d0d4bcd17a192bfa7bb17c
Swati <swati.pipariya6171@gmail.com>
2024-05-03 19:04:04 +0530

    Home Section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import str from "../Images/str.jpg"
 
 function Home() {
   return (
-    <div>
-      {/* Home */}
+    <div >
+      {/* -------------Top Image----------------- */}
+      <div>
+        <img src={str} alt='Front' className='w-full h-1/4 gap-3 sm:w-full' />
+        <div className=' w-3/6 p-12 gap-40'>
+          <h2 className='text-7xl font-bold sm:text-4xl'>Summer Collection</h2>
+          <p className='text-lg mt-3'>Embrace the vibrant hues and breezy fabrics of our Summer Collection, designed to infuse your wardrobe with effortless elegance under the sun's warm embrace.</p>
+            <div className='bg-black w-30 p-4 h-13 absolute top-70 text-white mt-2'>
+              Shop Now!
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,55 +1,117 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { IoSearch } from "react-icons/io5";
-import { FaFacebookF } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-
+import React, { useState } from 'react'
 
 const Navbar = () => {
-
-const[isOpen, setIsOpen] = useState(false);
-
-const toggleOpen = () => {
-   setIsOpen(!isOpen)
-}
-
+    let Links =[
+      {name:"HOME",link:"/"},
+      {name:"PAGES",link:"/pages"},
+      {name:"PRODUCTS",link:"/products"},
+      {name:"BLOG'S",link:"/blog"},
+      {name:"CONTACT",link:"/contact"},
+    ];
+    let [open,setOpen]=useState(false);
   return (
-    <nav className="flex justify-between font-sans text-xl items-center  py-4 px-6">
-
-      {/* Logo on the left */}
-      <div className="flex items-center">
-        <img src="/path/to/your/logo.png" alt="Logo" className="h-8 mr-2" />
+    <div className='shadow-md w-full sticky top-0 left-0 '>
+      <div className='md:flex items-center justify-between bg-blue py-4 md:px-10 px-7 '>
+      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800 '>
+        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
+        <ion-icon name="logo-ionic"></ion-icon>
+        </span>
+         E-Commerce
+      </div>
+      
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden '>
+      <ion-icon name={open ? 'close':'menu'}></ion-icon>
       </div>
 
-      {/* Hamburger functionality */}
-      <div className="block lg:hidden static">
-        <button onClick={toggleOpen} className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600">
-          <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            {isOpen ? (
-              <path fillRule="evenodd" clipRule="evenodd" d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 5H4v2h16v-2z" />
-            ) : (
-              <path fillRule="evenodd" clipRule="evenodd" d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z" />
-            )}
-          </svg>
-        </button>
-      </div>
-
-      {/* Navbar links on the right */}
-      {/* <ul className="flex font-mono font-bold "> */}
-      <ul className={`lg:flex font-mono font-bold ${isOpen ? 'block' : 'hidden'}`}>
-
-        <li className="mr-8"><Link to="/">HOME</Link></li>
-        <li className="mr-8"><Link to="/pages">PAGES</Link></li>
-        <li className="mr-8"><Link to="/products">PRODUCTS</Link></li>
-        <li className="mr-8"><Link to="/blog">BLOG</Link></li>
-        <li className='mr-5'><Link to="/contact">CONTACT</Link></li>
-        <li className='w-8 h-8 flex justify-center items-center bg-slate-200 rounded-full mr-2'><FaTwitter className='w-6 h-6 m-auto '/></li>
-        <li className='w-8 h-8 flex justify-center items-center bg-slate-200 rounded-full mr-2'><FaFacebookF/></li>
-        <li className='w-8 h-8 flex justify-center items-center bg-slate-200 rounded-full '><IoSearch className='w-6 h-6 m-auto '/></li>
-
+      <ul className={` md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in  ${open ? 'top-20 ':'top-[-490px]'}`}>
+        {
+          Links.map((link)=>(
+            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7 font-semibold'>
+              <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+            </li>
+          ))
+        }
+        {/* <Button>
+          Get Started
+        </Button> */}
       </ul>
-    </nav>  
-  );
+      </div>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { IoSearch } from "react-icons/io5";
+// import { FaFacebookF } from "react-icons/fa";
+// import { FaTwitter } from "react-icons/fa";
+
+
+// const Navbar = () => {
+
+// const[isOpen, setIsOpen] = useState(false);
+
+// const toggleOpen = () => {
+//    setIsOpen(!isOpen)
+// }
+
+//   return (
+//     <nav className="flex justify-between font-sans text-xl items-center  py-4 px-6">
+
+//       {/* Logo on the left */}
+//       <div className="flex items-center ">
+//         <img src="/path/to/your/logo.png" alt="Logo" className="h-8 mr-2 " />
+//       </div>
+
+//       {/* Hamburger functionality */}
+//       <div className="block lg:hidden static">
+//         <button onClick={toggleOpen} className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600 ">
+//           <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+//             {isOpen ? (
+//               <path fillRule="evenodd" clipRule="evenodd" d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 5H4v2h16v-2z" />
+//             ) : (
+//               <path fillRule="evenodd" clipRule="evenodd" d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z" />
+//             )}
+//           </svg>
+//         </button>
+//       </div>
+
+//       {/* Navbar links on the right */}
+//       {/* <ul className="flex font-mono font-bold "> */}
+//       <ul className={`lg:flex font-mono font-bold ${isOpen ? 'block' : 'hidden'}`}>
+
+//         <li className="mr-8"><Link to="/">HOME</Link></li>
+//         <li className="mr-8"><Link to="/pages">PAGES</Link></li>
+//         <li className="mr-8"><Link to="/products">PRODUCTS</Link></li>
+//         <li className="mr-8"><Link to="/blog">BLOG</Link></li>
+//         <li className='mr-5'><Link to="/contact">CONTACT</Link></li>
+//         <li className='w-8 h-8 flex justify-center items-center bg-slate-200 rounded-full mr-2'><FaTwitter className='w-6 h-6 m-auto '/></li>
+//         <li className='w-8 h-8 flex justify-center items-center bg-slate-200 rounded-full mr-2'><FaFacebookF/></li>
+//         <li className='w-8 h-8 flex justify-center items-center bg-slate-200 rounded-full '><IoSearch className='w-6 h-6 m-auto '/></li>
+
+//       </ul>
+//     </nav>  
+//   );
+// }
+
+// export default Navbar;
